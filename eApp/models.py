ea34dcd4300a6037e77ccb5f472da0c06a0ca17e
Ouko Ezra <oukoezra@Oukos-MacBook-Pro.local>
2023-01-30 17:49:13 +0300

    product model

--- eApp/models.py
+++ eApp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    category = models.CharField(max_length=255)
+    price = models.IntegerField()
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.name
+
+    
+    class Meta:
+        ordering = ['name']
