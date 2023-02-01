6bf2e9f74d15102e9dfa5c56fc13915113afef78
Ouko Ezra <oukoezra@Oukos-MacBook-Pro.local>
2023-02-01 09:09:09 +0300

    views

--- eApp/views.py
+++ eApp/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render
-from eApp.models import Product
+from rest_framework import viewsets, status
+from eApp.models import Product, Order
+from eApp.serializers import ProductSerializer, OrderSerializer
+from rest_framework.response import Response
 
 # Create your views here.
+class ProductViewset(viewsets.Viewset):
+    def list(self, request):
+        queryset = Product.objects.all()
+
+        serialized_data = ProductSerializer(queryset, many=True)
+
+        # return data / response
+        if not serialized_data.data:
+            return Response(
+                {'detail': [], 'code':404},
+                status=status.HTTP_200_OK
+            )
+        
+        return Response(
+            {'detail': serialized_data.data, 'code':200},
+            status=status.HTTP_200_OK
+        )
